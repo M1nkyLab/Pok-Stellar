434eb7ed00a0695fc8055ee4bf4165ce7bd4e956
M1nky <kl2311015157@student.uptm.edu.my>
2025-01-31 20:49:51 +0800

    2nd commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./*.html"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+
+  tailwind.config = {
+    darkMode: 'class',
+    theme: {
+      extend: {}
+    }
+  }
